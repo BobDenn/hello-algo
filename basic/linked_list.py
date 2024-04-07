cd135d81d8d92cf0ee6a4bbf56c3e14acc52ddd2
DK903 <1137005673@qq.com>
2024-04-07 14:24:05 +0800

    insert_head  completed

--- basic/linked_list.py
+++ basic/linked_list.py
@@ -4,11 +4,22 @@ class ListNode:
         self.val: int = val
         self.next: ListNode | None = None
 
+class LinkList:
+    """链表实例"""
+    def __init__(self) -> None:
+        self.head = None
+
+    def insert_head(n: ListNode, val: int) -> ListNode:
+        new_node = ListNode(val)
+        new_node.next = n
+        return new_node
     
-def insert(n0: ListNode, P: ListNode):
-    """在链表的 n0 节点插入节点P"""
-    P.next = n1
-    n0.next = P
+    def insert_tail(self) -> ListNode:
+        pass
+    
+    def insert_middle(self) -> ListNode:
+        pass
+    
 
 # 初始化链表 1 -> 3 -> 2 -> 5 -> 4
 n0 = ListNode(1)
@@ -16,17 +27,17 @@ n1 = ListNode(3)
 n2 = ListNode(2)
 n3 = ListNode(5)
 n4 = ListNode(4)
-P = ListNode(9)
 # 构建引用指向
 n0.next = n1
 n1.next = n2
 n2.next = n3
 n3.next = n4
 # 插入节点
-insert(n0, P)
+new_n0 = LinkList.insert_head(n0, 9)
 # 遍历链表
-current_node = n0
+current_node = new_n0
+values = []
 while current_node is not None:
-    print(current_node.val, end=' ')
+    values.append(current_node.val)
     current_node = current_node.next
-
+print(f"链表中有{values}")
