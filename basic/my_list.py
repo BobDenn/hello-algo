b40cbd92b45c415ce5fbc12137bf741f5ae8c3fb
DK903 <1137005673@qq.com>
2024-04-07 18:07:45 +0800

    finished

--- basic/my_list.py
+++ basic/my_list.py
@@ -19,7 +19,7 @@ class MyList:
         """获取列表容量"""
         return self._capacity
 
-    def get(self, index: int):
+    def get(self, index: int) -> int:
         """获取元素"""
         if index < 0 or index > self._size:
             raise IndexError("索引越界")
@@ -44,4 +44,31 @@ class MyList:
         self._arr[self._size] = num
         self._size += 1
 
+    def insert(self, index: int, num: int):
+        """插入元素"""
+        if index < 0 or index > self._size:
+            raise IndexError("索引越界")
+        if self._size == self._capacity:
+            self.extend_capacity()
+        for i in range(self._size -1, index-1, -1):
+            self._arr[i+1] = self._arr[i]
+        # 把 num 插在 index 处
+        self._arr[index] = num
+        self._size += 1
+    
+    def remove(self, index) -> int:
+        """删除元素"""
+        if index < 0 or index > self._size:
+            raise IndexError("索引越界")
+        num = self._arr[index]
+        for i in range(index, self._size -1):
+            self._arr[i] = self._arr[i+1]
+        # 更新元素数量
+        self._size -= 1
+        # 返回被删除的元素
+        return num
+      
+    def see_arr(self) -> list[int]:
+        """返回有效长度的列表"""
+        return self._arr[: self._size]
     
