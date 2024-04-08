3c0425b98acc1e4825de1e36f023f68765830a13
BobDenn <1137005673@qq.com>
2024-04-08 15:41:50 +0800

    small changes

--- Python/class_practice1.py
+++ Python/class_practice1.py
@@ -5,11 +5,11 @@ class Person:
 
     def run(self):
         self.weight -= 0.5
-        print(f"{self.name}跑步了，体重减少了0.5公斤")
+        print(f"{self.name}跑步了,体重减少了0.5公斤")
 
     def eat(self):
         self.weight += 1
-        print(f"{self.name}大餐了，体重增加了1公斤")
+        print(f"{self.name}大餐了,体重增加了1公斤")
 
     def __str__(self):
         return f"{self.name}目前的体重为{self.weight}公斤"
--- basic/linked_list.py
+++ basic/linked_list.py
@@ -42,30 +42,32 @@ def findN(n: ListNode, target: int) -> int:
         n = n.next
         index += 1
 
-# 初始化链表 1 -> 3 -> 2 -> 5 -> 4
-n0 = ListNode(1)
-n1 = ListNode(3)
-n2 = ListNode(2)
-n3 = ListNode(5)
-n4 = ListNode(4)
-# 构建引用指向
-n0.next = n1
-n1.next = n2
-n2.next = n3
-n3.next = n4
-# 插入节点
-new_head = LinkList.insert_head(n0, 0)
-nx = LinkList.insert_middle(n2,7)
-new_tail = LinkList.insert_tail(n4, 9)
-# 删除节点
-LinkList.remove(n2)
-# 查找节点
-findN(new_head, 9)
-# 遍历链表
-current_node = new_head
-values = []
-while current_node is not None:
-    values.append(current_node.val)
-    current_node = current_node.next
-print(f"链表中有{values}")
-# 链表中有[0, 1, 3, 2, 7, 5, 4, 9]
+
+if __name__ == "__main__": 
+    # 初始化链表 1 -> 3 -> 2 -> 5 -> 4
+    n0 = ListNode(1)
+    n1 = ListNode(3)
+    n2 = ListNode(2)
+    n3 = ListNode(5)
+    n4 = ListNode(4)
+    # 构建引用指向
+    n0.next = n1
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    # 插入节点
+    new_head = LinkList.insert_head(n0, 0)
+    nx = LinkList.insert_middle(n2,7)
+    new_tail = LinkList.insert_tail(n4, 9)
+    # 删除节点
+    LinkList.remove(n2)
+    # 查找节点
+    findN(new_head, 9)
+    # 遍历链表
+    current_node = new_head
+    values = []
+    while current_node is not None:
+        values.append(current_node.val)
+        current_node = current_node.next
+    print(f"链表中有{values}")
+    # 链表中有[0, 1, 3, 2, 7, 5, 4, 9]
