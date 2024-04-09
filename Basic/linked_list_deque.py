fcf2fbf2ac5e3bea4dee3356a074ff7b81989aab
BobDenn <1137005673@qq.com>
2024-04-09 17:38:20 +0800

    linked_list_deque

--- Basic/linked_list_deque.py
+++ Basic/linked_list_deque.py
@@ -29,3 +29,64 @@ class LinkedListDeque:
             raise IndexError("栈为空")
         return self._rear.val
     
+    def push(self, val: int, is_front: bool):
+        """in deque"""
+        node = ListNode(val)
+        # 若链表为空 头节点和尾节点 都指向 node
+        if self.is_empty():
+            self._front = self._rear = node
+        elif is_front:
+            self._front.prev = node
+            node.next = self._front
+            self._front = node
+        else:
+            self._rear.next = node
+            node.prev = self._rear
+            self._rear = node
+        self._size += 1
+
+    def push_first(self, val: int):
+        """队首入队"""
+        self.push(val, True)
+    
+    def push_last(self, val: int):
+        """队尾入队"""
+        self.push(val, False)
+
+    def pop(self, is_front: bool):
+        """出队"""
+        if self.is_empty():
+            raise IndexError("双向队列为空")
+        if is_front:
+            # 暂存头节点
+            val: int = self._front.val
+            fnext: ListNode | None = self._front.next
+            if fnext != None:
+                fnext.prev = None
+                self._front.next = None
+            self._front = fnext
+        else:
+            val: int = self._rear.val
+            rprev: ListNode | None = self._rear.prev
+            if rprev != None:
+                rprev.next = None
+                self._rear.prev = None
+            self._rear = rprev
+        self._size -= 1
+        return val
+
+    def pop_first(self) -> int:
+        """队首出队"""
+        return self.pop(True)
+
+    def pop_last(self) -> int:
+        """队尾出队"""
+        return self.pop(False)
+    
+    def to_list(self) -> list[int]:
+        res = [0] * self.size()
+        node = self._front
+        for i in range(self.size()):
+            res[i] = node.val
+            node = node.next
+        return res
