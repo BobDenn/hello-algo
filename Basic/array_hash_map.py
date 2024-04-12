de9ea45e8ef200668c89245abe338756ebd14df1
BobDenn <1137005673@qq.com>
2024-04-12 12:45:03 +0800

    finished

--- Basic/array_hash_map.py
+++ Basic/array_hash_map.py
@@ -1,6 +1,6 @@
 class key_val:
     """键值对"""
-    def __init__(self, key: int, val: str) -> None:
+    def __init__(self, key: int, val: str):
         self.key = key
         self.val = val
 
@@ -9,7 +9,7 @@ class ArrHashMap:
     """基于数组实现的哈希表"""
     def __init__(self) -> None:
         # 初始化桶的容量
-        self.buckets = list[key_val | None] = [None] * 100
+        self.buckets: list[key_val | None] = [None] * 100
 
     def hash_f(self, key: int) -> int:
         """通过key获取index索引"""
@@ -53,3 +53,26 @@ class ArrHashMap:
                 res.append(key_val.key)
         return res
     
+    def all_value(self) -> list[str]:
+        """"获取所有的值"""    
+        res: list[str] = []
+        for key_val in self.buckets:
+            if key_val is not None:
+                res.append(key_val.val)
+        return res
+    
+    def print_h(self):
+        """打印哈希表"""
+        for key_val in self.buckets:
+            if key_val is not None:
+                print(key_val.key, "->", key_val.val)
+
+
+if __name__ == '__main__':
+    myhash = ArrHashMap()
+    myhash.put(12836, "小哈")
+    myhash.put(15937, "小啰")
+    myhash.put(16750, "小算")
+    myhash.put(13276, "小法")
+    myhash.put(10583, "小鸭")
+    myhash.print_h()
