a034724849f7bd3f24fa70c875775520f3b4391e
BobDenn <1137005673@qq.com>
2024-04-15 09:37:07 +0800

    修改key_val -> Pair

--- Basic/array_hash_map.py
+++ Basic/array_hash_map.py
@@ -1,4 +1,4 @@
-class key_val:
+class Pair:
     """键值对"""
     def __init__(self, key: int, val: str):
         self.key = key
@@ -9,7 +9,7 @@ class ArrHashMap:
     """基于数组实现的哈希表"""
     def __init__(self) -> None:
         # 初始化桶的容量
-        self.buckets: list[key_val | None] = [None] * 100
+        self.buckets: list[Pair | None] = [None] * 100
 
     def hash_f(self, key: int) -> int:
         """通过key获取index索引"""
@@ -20,14 +20,14 @@ class ArrHashMap:
         """获取值"""
         index: int = self.hash_f(key)
         # 变量couple 存储键值对
-        couple: key_val = self.buckets[index]
+        couple: Pair = self.buckets[index]
         if couple is None:
             print("该值为空")
         return couple.val
     
     def put(self, key: int, val: str):
         """添加键值对"""
-        couple = key_val(key, val)
+        couple = Pair(key, val)
         index = self.hash_f(key)
         self.buckets[index] = couple
         
@@ -37,35 +37,35 @@ class ArrHashMap:
         # 置为None, 表示删除
         self.buckets[index] = None
     
-    def all_key_val(self) -> list[key_val]:
+    def all_Pair(self) -> list[Pair]:
         """获取所有的键值对"""
         res = []
-        for key_val in self.buckets:
-            if key_val is not None:
-                res.append(key_val)
+        for Pair in self.buckets:
+            if Pair is not None:
+                res.append(Pair)
         return res
     
     def all_key(self) -> list[int]:
         """获取所有的键"""
         res: list[int] = []
-        for key_val in self.buckets:
-            if key_val is not None:
-                res.append(key_val.key)
+        for Pair in self.buckets:
+            if Pair is not None:
+                res.append(Pair.key)
         return res
     
     def all_value(self) -> list[str]:
         """"获取所有的值"""    
         res: list[str] = []
-        for key_val in self.buckets:
-            if key_val is not None:
-                res.append(key_val.val)
+        for Pair in self.buckets:
+            if Pair is not None:
+                res.append(Pair.val)
         return res
     
     def print_h(self):
         """打印哈希表"""
-        for key_val in self.buckets:
-            if key_val is not None:
-                print(key_val.key, "->", key_val.val)
+        for Pair in self.buckets:
+            if Pair is not None:
+                print(Pair.key, "->", Pair.val)
 
 
 if __name__ == '__main__':
