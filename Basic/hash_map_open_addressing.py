f44a3958a5bb38cb2725c14ab36e94603d073f00
BobDenn <1137005673@qq.com>
2024-04-18 10:40:33 +0800

    更改目录

--- Basic/hash_map_open_addressing.py
+++ Basic/hash_map_open_addressing.py
@@ -1,4 +1,16 @@
+import array_hash_map as kv
+
+
 class HashMapOpenAddressing:
     """开放寻址哈希表"""
-    pass
+    def __init__(self):
+        """构造方法"""
+        self.size = 0 # 键值对数量
+        self.capacity = 4 # 表容量
+        self.load_thres = 2.0 / 3.0 # 扩容阈值
+        self.extend_ratio = 2 # 扩容倍数
+        self.buckets: list[kv.Pair | None] = [None] * self.capacity # 桶数组
+        self.EMPTY = kv.Pair(-1,'-1') # 懒删除机制的删除标记
+        
+    
     
