27999a233a343ca2cc061a923c2ba02b4a59ed9c
BobDenn <1137005673@qq.com>
2024-04-30 10:39:52 +0800

    def insert()

--- Basic/binary_search_tree.py
+++ Basic/binary_search_tree.py
@@ -8,24 +8,49 @@ class TreeNode:
 
 class BinarySearchTree:
     """二叉搜索树"""
-    def __init__(self, root) -> None:
+    def __init__(self, root: TreeNode) -> None:
         self._root: TreeNode = root
 
+    def search(self, num: int) -> TreeNode | None:
+        """查找节点"""
+        cur = self._root
+        # 循环查找, 越过叶节点跳出
+        while cur is not None:
+            # 目标节点在 cur 的右子树中
+            if cur.val < num:
+                cur = cur.right
+            # 目标节点在 cur 的左子树中
+            elif cur.val > num:
+                cur = cur.left
+            # 找到就跳出循环
+            else:
+                break
+        # 返回搜索到的值
+        return cur
 
-def search(self, num: int) -> TreeNode | None:
-    """查找节点"""
-    cur = BinarySearchTree._root
-    # 循环查找, 越过叶节点跳出
-    while cur is not None:
-        # 目标节点在 cur 的右子树中
-        if cur.val < num:
-            cur = cur.right
-        # 目标节点在 cur 的左子树中
-        elif cur.val > num:
-            cur = cur.left
-        # 找到就跳出循环
+    def insert(self, num:int):
+        """插入节点"""
+        # 若树为空, 则初始化根节点
+        if self._root is None:
+            self._root = TreeNode(num)
+            return
+        # 循环查找, 越过叶节点后跳出
+        cur, pre = self._root, None
+        while cur is not None:
+            # 如果找到重复节点, 直接返回
+            if num == cur.val:
+                return
+            pre = cur
+            # 插入位置在cur的右子树中
+            if num > cur.val:
+                cur = cur.right
+            # 插入位置在cur的左子树中
+            else:
+                cur = cur.left
+        # 插入节点
+        node = TreeNode(num)
+        if num < pre.val:
+            pre.left = node
         else:
-            break
-    # 返回搜索到的值
-    return cur
+            pre.right = node
 
