effa1cceaca659f45af3bdaf84ad16602c36bee9
BobDenn <1137005673@qq.com>
2024-05-06 18:02:53 +0800

    tmp

--- Basic/binary_search_tree.py
+++ Basic/binary_search_tree.py
@@ -53,4 +53,29 @@ class BinarySearchTree:
             pre.left = node
         else:
             pre.right = node
+            
+    def remove(self, num: int):
+        """ 删除节点 """
+        # 若树为空, 直接提前返回
+        if self._root is None:
+            return
+        # 循环查找, 越过叶节点后跳出
+        cur, Pre = self._root, None
+        while cur is not None:
+            # 找到待删除节点, 跳出循环
+            if cur.val == num:
+                break
+            pre = cur
+            # 待删除节点在cur的右子树中
+            if cur.val < num:
+                cur = cur.right
+            # 待删除节点在cur的左子树中
+            else:
+                cur = cur.left
+        # 若无待删除节点, 则直接返回
+        if cur is None:
+            return
+        # 子节点数量 = 0 / 1
+        if cur.left is None or cur.right is None:
+            # 当子节点数量
 
