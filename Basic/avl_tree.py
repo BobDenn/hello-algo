c0e75aafb9018789ee9ed5a768b2943661bd5c64
BobDenn <1137005673@qq.com>
2024-05-09 17:33:23 +0800

    modified sth

--- Basic/avl_tree.py
+++ Basic/avl_tree.py
@@ -1,6 +1,6 @@
 class Avl_tree:
     """平衡二叉树"""
-    class Treenode:
+    class node:
         """AVL 树节点类""" 
         def __init__(self, val: int) -> None:
             self.val = val
@@ -9,20 +9,20 @@ class Avl_tree:
             self.right = None
     
     @staticmethod
-    def height(node: Treenode | None) -> int:
+    def height(node: "Avl_tree.node") -> int:
         """获取节点高度"""
         # 增加判断是否为空
         if node is not None:
             return node.height
         return -1
 
-    def update_height(self, node: Treenode | None):
+    def update_height(self, node: "Avl_tree.node" | None):
         """更新节点高度"""
         # 节点高度 = 最高子树 + 1
         node.height = max([self.height(self.left)],
                           [self.height(self.right)]) + 1
         
-    def balance_factor(self, node) -> int:
+    def balance_factor(self, node: "Avl_tree.node" | None) -> int:
         """获取平衡因子"""
         # 空节点就是 0
         if node is None:
