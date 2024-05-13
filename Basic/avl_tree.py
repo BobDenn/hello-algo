eccaf5b45d8b088a9906e8a5986dc8326ba8b31e
BobDenn <1137005673@qq.com>
2024-05-13 14:37:17 +0800

    quick

--- Basic/avl_tree.py
+++ Basic/avl_tree.py
@@ -8,19 +8,19 @@ class Avl_tree:
             self.left = None
             self.right = None
     
-    @staticmethod
     def height(node: "Avl_tree.node") -> int:
         """获取节点高度"""
         # 增加判断是否为空
         if node is not None:
             return node.height
-        return -1
+        else:
+            return -1
 
     def update_height(self, node: "Avl_tree.node" | None):
         """更新节点高度"""
         # 节点高度 = 最高子树 + 1
-        node.height = max([self.height(self.left)],
-                          [self.height(self.right)]) + 1
+        node.height = max([self.height(self.node.left)],
+                          [self.height(self.node.right)]) + 1
         
     def balance_factor(self, node: "Avl_tree.node" | None) -> int:
         """获取平衡因子"""
@@ -30,4 +30,27 @@ class Avl_tree:
         # 节点平衡因子 = 左子树 - 右子树
         return self.height(node.left) - self.height(node.right)
         
-    
+    def right_rotate(self, node: "Avl_tree.node" | None) -> node | None:
+        """node中存放失衡节点, 右旋操作"""
+        child = node.left
+        grand_child = child.right
+        # 以child为原点, 将node向右旋转
+        child.right = node
+        node.left = grand_child
+        # 更新节点高度
+        self.update_height(node)
+        self.update_height(child)
+        return child
+
+    def left_rotate(self, node: "Avl_tree.node" | None) -> node | None:
+        """node中存放失衡节点, 左旋操作"""
+        child = node.right
+        grand_child = child.left
+        # 以child为原点, 将node旋转
+        child.left = node
+        node.right = grand_child
+        # 更新节点高度
+        self.update_height(node)
+        self.update_height(child)
+        return child
+
