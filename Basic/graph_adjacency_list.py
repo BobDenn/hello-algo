875d7c8c0b5fd472adaeae67ed644cf38363fcbf
BobDenn <1137005673@qq.com>
2024-05-30 17:55:33 +0800

    finished

--- Basic/graph_adjacency_list.py
+++ Basic/graph_adjacency_list.py
@@ -11,31 +11,35 @@ class GraphAdjList:
         """构造方法"""
         self.adj_list: dict[Vertex, list[Vertex]] = {}
         for edge in edges:
-            self.add_vertex(edge[0])
-            self.add_vertex(edge[1])
             self.add_edge(edge[0], edge[1])
             
     def size(self):        
         return len(self.adj_list)
     
     def add_edge(self, vet1: Vertex, vet2: Vertex):
-        if vet1 not in self.adj_list or vet2 not in self.adj_list or vet1 == vet2:
-            raise IndexError()
+        """添加一条无向边"""
+        if vet1 == vet2:
+            raise IndexError("不能连接顶点自身")
+        self.add_vertex(vet1)
+        self.add_vertex(vet2)
         self.adj_list[vet1].append(vet2)
         self.adj_list[vet2].append(vet1)
         
     def remove_edge(self, vet1: Vertex, vet2: Vertex):
+        """移除一条无向边"""
         if vet1 not in self.adj_list or vet2 not in self.adj_list or vet1 == vet2:
-            raise IndexError()
+            raise ValueError("顶点不在图中")
         self.adj_list[vet1].remove(vet2)
         self.adj_list[vet2].remove(vet1)
             
-    def add_vertex(self, vet: Vertex):            
+    def add_vertex(self, vet: Vertex):
+        """添加一个顶点"""
         if vet in self.adj_list:
             return
         self.adj_list[vet] = []
         
     def remove_vertex(self, vet: Vertex):
+        """移除一个顶点"""
         if vet not in self.adj_list:
             raise ValueError()
         # 删除顶点
@@ -45,9 +49,21 @@ class GraphAdjList:
             if vet in self.adj_list[vertex]:
                 self.adj_list[vertex].remove(vet)
                 
-    def print(self):
+    def print_adj_list(self):
+        """打印邻接表"""
         print("邻接表= ")
         for vertex in self.adj_list:
-            tmp = [v.val for v in self.adj_list[vertex]]
-            print(f"{vertex.val}: {tmp},")
+            adjacent_vertices = [v.value for v in self.adj_list[vertex]]
+            print(f"{vertex.value}: {adjacent_vertices},")
             
+            
+if __name__ == "__main__":
+    v1 = Vertex(1)
+    v2 = Vertex(2)
+    v3 = Vertex(3)
+    v4 = Vertex(4)
+    v5 = Vertex(5)
+    edges = [[v1, v2], [v2, v3], [v3, v4], [v4, v5], [v5, v1]]
+    graph = GraphAdjList(edges)
+    graph.print_adj_list()
+    
