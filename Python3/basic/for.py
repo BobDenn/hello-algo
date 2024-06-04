907adfe8783cc094385d51c04204cde49996cb40
BobDenn <1137005673@qq.com>
2024-06-04 22:16:45 +0800

    yeah baby

--- Python3/basic/for.py
+++
@@ -1,8 +0,0 @@
-num = 0
-count = 0
-# i 是变量
-for i in range(100):
-    print(i)
-    count += 1
-    num = num + count
-print(num)
--- Python3/basic/str_find.py
+++
@@ -1,9 +0,0 @@
-# 在一段字符串中查找某段字符，并返回其所在位置
-my_str = '黑马程序员'
-sub_str = '马'
-res = my_str.find(sub_str)
-# 若没有找到会返回-1
-if res != -1:
-    print(f'{sub_str}存在，它在{res}位置')
-else:
-    print(f"{sub_str}不存在")
