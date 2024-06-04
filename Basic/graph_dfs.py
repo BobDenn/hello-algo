23911bb87ca5129651727d1f233112ac312bf4e1
BobDenn <1137005673@qq.com>
2024-06-04 16:32:32 +0800

    finished

--- Basic/graph_dfs.py
+++ Basic/graph_dfs.py
@@ -10,3 +10,12 @@ def dfs(graph: GraphAdjList, visited: set[Vertex], res: list[Vertex], vet: Verte
         if adjVet in visited:
             continue
         dfs(graph, visited, res, vet) # 递归访问顶点
+        
+
+def graph_dfs(graph: GraphAdjList, start_vet: Vertex) -> list[Vertex]:
+    """深度优先遍历 DFS"""
+    res = []
+    # 哈希表, 用于记录已被访问过的顶点
+    visited = set[Vertex]()
+    dfs(graph, visited, res, start_vet)
+    return res
